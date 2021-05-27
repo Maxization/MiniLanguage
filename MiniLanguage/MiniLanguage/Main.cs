5c0b645666fd3512cb5ed90b50bc5691ec041b36
maxization <damian.opoka99@gmail.com>
2021-05-27 20:53:01 +0200

    Init

---
+++ MiniLanguage/MiniLanguage/Main.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace MiniLanguage
+{
+    class Compiler
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ MiniLanguage/MiniLanguage/kompilator.lex
@@ -0,0 +1,4 @@
+%using QUT.Gppg;
+
+%namespace MiniLanguage
+
